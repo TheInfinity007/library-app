398cd1ea96bb16a9b6a828cf7f50e75a8844633f
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-06 18:32:08 +0530

    spring app init

---
+++ library-app-service/src/main/java/com/love2code/library_app_service/LibraryAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.love2code.library_app_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppServiceApplication.class, args);
+	}
+
+}
---
+++ library-app-service/src/test/java/com/love2code/library_app_service/LibraryAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love2code.library_app_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
