be6cca1d6b17519e6f8e5c11dfc619ac3bc1dfcd
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-08 12:30:24 +0530

    add comments

--- library-app-service/src/main/java/com/love2code/library_app_service/dao/BookRepository.java
+++ library-app-service/src/main/java/com/love2code/library_app_service/dao/BookRepository.java
@@ -7,5 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    /*
+    In Spring Data JPA, query methods are derived based on the method name.
+    The method name is parsed by Spring Data, and
+    it automatically generates the query based on the name of the method
+
+    findBy: Indicates a search query (like a SELECT statement).
+    Title: Refers to the title field of the Book entity.
+    Containing: Informs Spring Data that the query should perform a partial match (LIKE) rather than an exact match (=).
+     */
     Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
