45f75bf74508daabeaeecb7839462147f00fd182
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-08 17:37:31 +0530

    fetch bookId from params

--- library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import BookModel from '../../models/BookModel';
 
 export const BookCheckoutPage = () => {
-
     const [book, setBook] = useState<BookModel>();
     const [isLoadingBook, setIsLoadingBook] = useState(true);
     const [httpError, setHttpError] = useState(null);
+
+    const bookId = window.location.pathname.split('/')[2];
+    console.log('bookID', bookId);
 
     return (
         <div>
