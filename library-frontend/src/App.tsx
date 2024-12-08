3c56dde1ee972abf25db074aeac04cb11c7d3eff
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-08 17:04:06 +0530

    Routes and Navigate to

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
     return (
         <>
             <Navbar />
             <Routes>
-                <Route path="/" element={<HomePage />}></Route>
-                <Route path="/search" element={<SearchBooksPage />}></Route>
+                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/search" element={<SearchBooksPage />} />
             </Routes>
             <Footer />
         </>
