805218ac1c925652479f223265dae10151b06332
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-08 17:44:03 +0530

    fetch the book data from the api

--- library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,13 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BookModel from '../../models/BookModel';
+import { SpinnerLoading } from '../Utils/SpinnerLoading';
 
 export const BookCheckoutPage = () => {
     const [book, setBook] = useState<BookModel>();
-    const [isLoadingBook, setIsLoadingBook] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
 
     const bookId = window.location.pathname.split('/')[2];
     console.log('bookID', bookId);
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+            const url: string = `${baseUrl}?page=0&size=9`;
+
+            const response: any = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const book = await response.json();
+
+            const loadedBook: BookModel = {
+                id: book.id,
+                title: book.title,
+                author: book.author,
+                description: book.description,
+                copies: book.copies,
+                copiesAvailable: book.copiesAvailable,
+                category: book.category,
+                img: book.img,
+            };
+
+            setBook(loadedBook);
+            setIsLoading(false);
+        };
+
+        fetchBook().catch((err: any) => {
+            setIsLoading(false);
+            setHttpError(err.message);
+        });
+    }, []);
+
+    // Handle Loading
+    if (isLoading) {
+        return <SpinnerLoading />;
+    }
+
+    // Hanlde Http Error
+    if (httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        );
+    }
 
     return (
         <div>
