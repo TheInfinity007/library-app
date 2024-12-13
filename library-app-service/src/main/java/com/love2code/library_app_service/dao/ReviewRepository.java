7c4f625fb2bcd8bf48fea09effc16d7ac53eba16
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-13 12:01:56 +0530

    add get reviews by bookid

--- library-app-service/src/main/java/com/love2code/library_app_service/dao/ReviewRepository.java
+++ library-app-service/src/main/java/com/love2code/library_app_service/dao/ReviewRepository.java
@@ -1,7 +1,12 @@
 package com.love2code.library_app_service.dao;
 
 import com.love2code.library_app_service.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@RequestParam("book_id") String bookId, Pageable pageable);
 }
