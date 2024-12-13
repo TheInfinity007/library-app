b327999c870a1f3f2060549cf054443d438684e7
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-13 19:55:14 +0530

    add okta config

---
+++ library-frontend/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+    issuer: `https://${process.env.REACT_APP_OKTA_TENANT}.okta.com/oauth2/default`,
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true
+}
