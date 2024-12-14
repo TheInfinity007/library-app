316ad9152aae570bb4a65c0fd8320e21dfd95f33
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-14 23:29:44 +0530

    add book checkout API

---
+++ library-app-service/src/main/java/com/love2code/library_app_service/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.love2code.library_app_service.controller;
+
+import com.love2code.library_app_service.entity.Book;
+import com.love2code.library_app_service.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@yopmail.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    };
+}
---
+++ library-app-service/src/main/java/com/love2code/library_app_service/service/BookService.java
@@ -0,0 +1,55 @@
+package com.love2code.library_app_service.service;
+
+import com.love2code.library_app_service.dao.BookRepository;
+import com.love2code.library_app_service.dao.CheckoutRepository;
+import com.love2code.library_app_service.entity.Book;
+import com.love2code.library_app_service.entity.Checkout;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class BookService {
+    private final BookRepository bookRepository;
+    private final CheckoutRepository checkoutRepository;
+
+    // Constructor dependency injection
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+        this.bookRepository = bookRepository;
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public Book checkoutBook(String userEmail, Long bookId) throws Exception {
+        Optional<Book> bookTemp = bookRepository.findById(bookId);
+        if (bookTemp.isEmpty()) {
+            throw new Exception("Book doesn't exist");
+        }
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        Book book = bookTemp.get();
+
+        if (validateCheckout != null || bookTemp.get().getCopiesAvailable() <= 0) {
+            throw new Exception("Book doesn't exist or already checked out by user");
+        }
+
+        book.setCopiesAvailable(book.getCopiesAvailable() - 1);
+        bookRepository.save(book);
+
+        Checkout checkout = new Checkout(
+                userEmail,
+                LocalDate.now().toString(),
+                LocalDate.now().plusDays(7).toString(),
+                book.getId()
+        );
+
+        checkoutRepository.save(checkout);
+
+        return book;
+    }
+
+    ;
+}
