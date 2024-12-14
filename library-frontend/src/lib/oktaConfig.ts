d3a8e78bb207c9ae9158e7f2fc233df9c5b26d37
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-14 15:59:34 +0530

    Add okta login and signin widget

--- library-frontend/src/lib/oktaConfig.ts
+++ library-frontend/src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
-    issuer: `https://${process.env.REACT_APP_OKTA_TENANT}.okta.com/oauth2/default`,
+    issuer: `https://${process.env.REACT_APP_OKTA_DOMAIN}.okta.com/oauth2/default`,
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
