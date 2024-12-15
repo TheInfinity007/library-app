5a84229d617e5fbfb95bff8a124b84061f6da139
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-15 21:28:49 +0530

    fix issuer and add base url

--- library-frontend/src/lib/oktaConfig.ts
+++ library-frontend/src/lib/oktaConfig.ts
@@ -1,6 +1,7 @@
 export const oktaConfig = {
+    baseUrl: `https://${process.env.REACT_APP_OKTA_DOMAIN}.okta.com`,
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
-    issuer: `https://${process.env.REACT_APP_OKTA_DOMAIN}.okta.com`,
+    issuer: `https://${process.env.REACT_APP_OKTA_DOMAIN}.okta.com/oauth2/default`,
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
