b60413b959ee2863e99421cecafa34e8ad63ad9f
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-15 19:05:27 +0530

    add security config

---
+++ library-app-service/src/main/java/com/love2code/library_app_service/config/SecurityConfiguration.java
@@ -0,0 +1,68 @@
+package com.love2code.library_app_service.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // Disable Cross Site Request Forgery
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // Protect endpoints at /api/<type>/secure/*
+//        http
+//                .securityMatcher("/api/books/secure/**")
+//                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+//                        .requestMatchers("/api/books/secure/**")
+//                        .authenticated()
+//                )
+//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer -> OAuth2ResourceServerConfigurer
+//                        .jwt()
+//                );
+
+//        http
+//                .authorizeRequests()
+//                .requestMatchers("/api/books/secure/**")
+//                .authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+
+        http
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/api/books/secure/**")
+                        .authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults())
+                );
+
+
+        // Add CORS filters
+        http.cors(httpSecurityCorsConfigurer -> {
+        });
+
+        // Add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+
+
+        // Force a non-empty response body for 401's to make the response friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
+    }
+}
