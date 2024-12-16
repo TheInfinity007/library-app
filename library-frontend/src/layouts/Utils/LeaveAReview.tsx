88220813aed22ed497f7fba70f7c917fcd45c2ad
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-16 16:10:26 +0530

    add review description component

--- library-frontend/src/layouts/Utils/LeaveAReview.tsx
+++ library-frontend/src/layouts/Utils/LeaveAReview.tsx
@@ -3,10 +3,15 @@ import { StarsReview } from './StarsReview';
 
 export const LeaveAReview: React.FC<{}> = (props) => {
     const [starInput, setStarInput] = useState(0);
+    const [displayInput, setDisplayInput] = useState(false);
+    const [reviewDescription, setReviewDescription] = useState('');
 
     const starValue = (value: number) => {
         setStarInput(value);
+        setDisplayInput(true);
     };
+
+    const starValues = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
 
     return (
         <div className="dropdown" style={{ cursor: 'pointer' }}>
@@ -17,19 +22,46 @@ export const LeaveAReview: React.FC<{}> = (props) => {
             >
                 Leave a review ?
             </h5>
-            <ul id="submitReviewRating" className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
-                <li><button onClick={()=> starValue(0.5)} className='dropdown-item'>0.5 star</button></li>
-                <li><button onClick={()=> starValue(1)} className='dropdown-item'>1 star</button></li>
-                <li><button onClick={()=> starValue(1.5)} className='dropdown-item'>1.5 star</button></li>
-                <li><button onClick={()=> starValue(2)} className='dropdown-item'>2 star</button></li>
-                <li><button onClick={()=> starValue(2.5)} className='dropdown-item'>2.5 star</button></li>
-                <li><button onClick={()=> starValue(3)} className='dropdown-item'>3 star</button></li>
-                <li><button onClick={()=> starValue(3.5)} className='dropdown-item'>3.5 star</button></li>
-                <li><button onClick={()=> starValue(4)} className='dropdown-item'>4 star</button></li>
-                <li><button onClick={()=> starValue(4.5)} className='dropdown-item'>4.5 star</button></li>
-                <li><button onClick={()=> starValue(5)} className='dropdown-item'>5 star</button></li>
+            <ul
+                id="submitReviewRating"
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+            >
+                {starValues.map((value) => (
+                    <li key={value}>
+                        <button
+                            onClick={() => starValue(value)}
+                            className="dropdown-item"
+                        >
+                            {value} star
+                        </button>
+                    </li>
+                ))}
             </ul>
-            <StarsReview rating={starInput} size={35}/>
+            <StarsReview rating={starInput} size={35} />
+
+            {displayInput && (
+                <form method="POST" action="#">
+                    <hr />
+                    <div className="mb-3">
+                        <label className="form-label">Description</label>
+                        <textarea
+                            className="form-control"
+                            id="submitReviewDescription"
+                            placeholder="Optional"
+                            rows={3}
+                            onChange={(e) =>
+                                setReviewDescription(e.target.value)
+                            }
+                        ></textarea>
+                    </div>
+                    <div>
+                        <button type="button" className="btn btn-primary mt-3">
+                            Submit Review
+                        </button>
+                    </div>
+                </form>
+            )}
         </div>
     );
 };
