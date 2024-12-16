4437a444c63f16c7d68ed868f5eb6248fd72d1ac
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-16 20:23:48 +0530

    update entity alias

--- library-app-service/src/main/java/com/love2code/library_app_service/dao/BookRepository.java
+++ library-app-service/src/main/java/com/love2code/library_app_service/dao/BookRepository.java
@@ -30,6 +30,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     */
     Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 
-    @Query("SELECT o FROM Book o WHERE id IN :book_ids")
+    @Query("SELECT b FROM Book b WHERE id IN :book_ids")
     List<Book> findBooksByBookIds(@Param("book_ids") List<Long> bookIds);
 }
