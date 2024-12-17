b01b0c1461ffd82ef8d146426d8699fffa639af4
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-17 22:45:02 +0530

    Add history click state

--- library-frontend/src/layouts/ShelfPage/ShelfPage.tsx
+++ library-frontend/src/layouts/ShelfPage/ShelfPage.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Loans } from './components/Loans';
 import { HistoryPage } from './components/HistoryPage';
 
 export const ShelfPage = () => {
+    const [historyClick, setHistoryClick] = useState(false);
+
     return (
         <div className="container">
             <div className="mt-3">
                 <nav>
                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
                         <button
+                            onClick={() => setHistoryClick(false)}
                             className="nav-link active"
                             id="nav-loans-tab"
                             data-bs-toggle="tab"
@@ -21,6 +24,7 @@ export const ShelfPage = () => {
                             Loans
                         </button>
                         <button
+                            onClick={() => setHistoryClick(true)}
                             className="nav-link"
                             id="nav-history-tab"
                             data-bs-toggle="tab"
@@ -50,7 +54,8 @@ export const ShelfPage = () => {
                         role="tabpanel"
                         aria-labelledby="nav-history-tab"
                     >
-                        <HistoryPage />
+                        {/* Load and Show history only when clicked */}
+                        {historyClick ? <HistoryPage /> : <></>}
                     </div>
                 </div>
             </div>
