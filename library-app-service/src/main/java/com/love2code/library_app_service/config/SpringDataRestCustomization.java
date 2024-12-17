e8c85b4b4c05282078d4338b0070f822058bf1f2
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-18 00:29:51 +0530

    add find message bt user email method and add security config

--- library-app-service/src/main/java/com/love2code/library_app_service/config/SpringDataRestCustomization.java
+++ library-app-service/src/main/java/com/love2code/library_app_service/config/SpringDataRestCustomization.java
@@ -1,6 +1,7 @@
 package com.love2code.library_app_service.config;
 
 import com.love2code.library_app_service.entity.Book;
+import com.love2code.library_app_service.entity.Message;
 import com.love2code.library_app_service.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -13,21 +14,23 @@ public class SpringDataRestCustomization implements RepositoryRestConfigurer {
     private String theAllowedOrigins = "http://localhost:3000";
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
-        HttpMethod[] theUnsupportedActions = { HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT };
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Review.class);
+        config.exposeIdsFor(Message.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
         disableHttpMethods(Review.class, config, theUnsupportedActions);
+        disableHttpMethods(Message.class, config, theUnsupportedActions);
 
         // Configure CORS Mapping
         cors.addMapping(config.getBasePath() + "/**")
                 .allowedOrigins(theAllowedOrigins);
     }
 
-    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions){
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
         config.getExposureConfiguration()
                 .forDomainType(theClass)
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
