c7bbd68b2256f565ee1482cf53b0680c2b95400b
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-17 13:34:04 +0530

    Add loans view for mobile

--- library-frontend/src/layouts/ShelfPage/components/Loans.tsx
+++ library-frontend/src/layouts/ShelfPage/components/Loans.tsx
@@ -115,6 +115,7 @@ export const Loans = () => {
                                         </div>
                                     </div>
                                 </div>
+                                <hr />
                             </div>
                         ))}
                     </>
@@ -129,6 +130,63 @@ export const Loans = () => {
             </div>
 
             {/* Mobile */}
+            <div className="container d-lg-none mt-2">
+                {shelfCurrentLoans.length > 0 ? (
+                    <>
+                        <h5 className='mb-3'>Current Loans:</h5>
+
+                        {shelfCurrentLoans.map((loan) => (
+                            <div key={loan.book.id}>
+                                <div className="d-flex justify-content-center align-items-center">
+                                    {loan.book.img ? (
+                                        <img
+                                            src={loan.book.img}
+                                            alt="Book"
+                                            width={226}
+                                            height={349}
+                                        />
+                                    ) : (
+                                        <img
+                                            src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+                                            alt="Book"
+                                            width={226}
+                                            height={349}
+                                        />
+                                    )}
+                                </div>
+                                <div className="card d-flex mt-5 mb-3">
+                                    <div className="card-body container">
+                                        <div className="mt-3">
+                                            <h4>Loan Options</h4>
+                                            {loan.daysLeft>0 && <p className="text-secondaary">Due in {loan.daysLeft} days.</p> }
+                                            {loan.daysLeft === 0 && <p className="text-success">Due Today.</p> }
+                                            {loan.daysLeft <= 0 && <p className="text-danger">Overdue by {Math.abs(loan.daysLeft)} days.</p> }
+                                            <div className="list-group mt-3">
+                                                <button className="list-group-item list-group-action-item" aria-current="true" data-bs-toggle="modal" data-bs-target={`#mobilemodal${loan.book.id}`}>
+                                                    Manage Loan
+                                                </button>
+                                                <Link to={`/search`} className='list-group-item list-group-item-action'>Search more books?</Link>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                        <p className="mt-3">Help other find their adventure by reviewing your loan.</p>
+                                        <Link to={`/checkout/${loan.book.id}`} className='btn btn-primary'>Leave a review</Link>
+                                    </div>
+                                </div>
+                                
+                                <hr />
+                            </div>
+                        ))}
+                    </>
+                ) : (
+                    <>
+                        <h3 className="mt-3">
+                            Currently no loans
+                        </h3>
+                        <Link to={`/search`} className='btn btn-primary'>Search for a new book</Link>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
