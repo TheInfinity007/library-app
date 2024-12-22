019cb3f0b2ac1195e6d5adcdbee21bf669866be6
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-22 17:22:03 +0530

    Add payment entity and repository

--- library-app-service/src/main/java/com/love2code/library_app_service/LibraryAppServiceApplication.java
+++ library-app-service/src/main/java/com/love2code/library_app_service/LibraryAppServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryAppServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryAppServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAppServiceApplication.class, args);
+    }
 
 }
