b48974a2a02c693a449092e8bb639b8c4217636e
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-12-23 11:32:21 +0530

    configured stripe root

--- library-frontend/src/config/index.ts
+++ library-frontend/src/config/index.ts
@@ -1,1 +1,2 @@
 export const BE_BASE_URL = process.env.REACT_APP_BE_BASE_URL || 'https://localhost:8443';
+export const STRIPE_PUBLISHABLE_KEY: string = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || '';
--- library-frontend/src/index.tsx
+++ library-frontend/src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 import './index.css';
 import { App } from './App';
+import { STRIPE_PUBLISHABLE_KEY } from './config';
+
+const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <BrowserRouter>
-        <App />
+        <Elements stripe={stripePromise}>
+            <App />
+        </Elements>
     </BrowserRouter>
 );
